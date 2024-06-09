c44ca9eb8db014b7df82fa57b92666586a8c4069
suxinying <suxinying@outlook.com>
2024-06-09 16:05:32 +0800

    1、增加demo

---
+++ yudao-module-demo/yudao-module-demo-biz/src/main/java/cn/iocoder/yudao/module/demo/DemoServerApplication.java
@@ -0,0 +1,18 @@
+package cn.iocoder.yudao.module.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目的启动类
+ *
+ * @author 芋道源码
+ */
+@SpringBootApplication
+public class DemoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServerApplication.class, args);
+    }
+
+}
